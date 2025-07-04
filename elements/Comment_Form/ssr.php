e616b17ec18778295b44b7f99f141aa455ec625b
Deepak Divakar <deepak112358@gmail.com>
2025-07-04 12:45:17 +0530

    More design changes to comments form

--- elements/Comment_Form/ssr.php
+++ elements/Comment_Form/ssr.php
@@ -5,7 +5,7 @@ $comment_form_args = array(
     'title_reply_after' => '</h5>',
     'class_form' => 'breakdance-form breakdance-form--comments',
     'class_submit' => 'comment-form__submit button-atom button-atom--primary breakdance-form-button',
-    'comment_field' => '<div class="breakdance-form-field breakdance-form-field--textarea"><label class="breakdance-form-field__label" for="comment">' . _x('Comment', 'noun') . '</label><textarea class="breakdance-form-field__input" id="comment" name="comment" aria-required="true"></textarea></div>',
+    'comment_field' => '<div class="breakdance-form-field breakdance-form-field--textarea"><label class="breakdance-form-field__label" for="comment">' . _x('Comment', 'noun') . ' <span class="required">*</span></label><textarea class="breakdance-form-field__input" id="comment" name="comment" aria-required="true"></textarea></div>',
     'submit_field' => '<div class="breakdance-form-field breakdance-form-footer">%1$s %2$s</div>',
     'fields' => apply_filters('comment_form_default_fields', array(
         
@@ -13,14 +13,12 @@ $comment_form_args = array(
         'author' =>
         '<div class="breakdance-form-fields-row">' .
         '<div class="breakdance-form-field breakdance-form-field--half">' .
-        '<label class="breakdance-form-field__label" for="author">' . __('Name') . '</label> ' .
-        ($req ? '<span class="required">*</span>' : '') .
+        '<label class="breakdance-form-field__label" for="author">' . __('Name') . ' <span class="required">*</span></label> ' .
         '<input id="author" class="breakdance-form-field__input" name="author" type="text" value="' . esc_attr($commenter['comment_author']) .
         '"' . $aria_req . ' /></div>',
 
         'email' =>
-        '<div class="breakdance-form-field breakdance-form-field--half"><label class="breakdance-form-field__label" for="email">' . __('Email') . '</label> ' .
-        ($req ? '<span class="required">*</span>' : '') .
+        '<div class="breakdance-form-field breakdance-form-field--half"><label class="breakdance-form-field__label" for="email">' . __('Email') . ' <span class="required">*</span></label> ' .
         '<input id="email" class="breakdance-form-field__input" name="email" type="text" value="' . esc_attr($commenter['comment_author_email']) .
         '"' . $aria_req . ' /></div>' .
         '</div>',
